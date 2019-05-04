e8b9f0dec40ca954718a49a98465de9af7919009
Ananyo Sen <ananyo@iitb.ac.in>
2019-05-04 22:40:46 +0530

    fix includes

--- include/domainparser.hpp
+++ include/domainparser.hpp
@@ -9,6 +9,6 @@
 #include <sstream>
 
 std::vector<std::string> split(const std::string& s);
-void DomainParser(std::string file_name,std::vector <double> &element_vector,unsigned int num_of_nodes,unsigned int num_of_elements);
+void DomainParser(std::string file_name,std::vector <FL_TYPE> &element_vector,unsigned int num_of_nodes,unsigned int num_of_elements);
 
 #endif
