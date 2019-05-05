d48015a27750c429352f68f2990df1b34d9d4bd6
sen <ananyo_aes@students.iiests.ac.in>
2019-05-05 17:21:34 +0000

    cuda render done

--- src/main.cu
+++ src/main.cu
@@ -24,17 +24,23 @@ std::vector<FL_TYPE> camera(3, 0);
 
 
 __global__ 
-void primary_rays(float* nodes,float* rays, float* illums, float* lights, size_t elem_size)
+void primary_rays(float* nodes,float* rays, float* image_plane, float* lights, size_t elem_size)
 {
     int _i = blockIdx.x * blockDim.x + threadIdx.x;
     // int _j = get_global_id(1);
     float ax,ay,az,bx,by,bz,cx,cy,cz,e01x,e01y,e01z,e02x,e02y,e02z,\
-    tx,ty,tz,u,v,D, Dy, Dz, t, nx, ny, nz, rox, roy, roz, rdx, rdy, rdz, ray_length_1;
+    tx,ty,tz,u,v,D, Dy, Dz, t, nx, ny, nz, rox, roy, roz, rdx, rdy, rdz, ray_length_1,\
+    srox, sroy, sroz, srdx, srdy, srdz, nl_1;
 
     float min_dis = 1e20;
-    int near_ele_num = -1;
+    int near_ele_num = -1, eid;
 
-    unsigned int sz, eidx;
+    unsigned int sz;//, eidx;
+
+    FL_TYPE lx = lights[0];
+    FL_TYPE ly = lights[1];
+    FL_TYPE lz = lights[2];
+
     
     __shared__ float elems_shared[100*9];
     
@@ -142,7 +148,7 @@ void primary_rays(float* nodes,float* rays, float* illums, float* lights, size_t
         }
         __syncthreads();
     }
-    __syncthreads();
+    
     // if(min_dis<1e19)
     // {
     //     is_hit[_i] = near_ele_num;
@@ -151,6 +157,59 @@ void primary_rays(float* nodes,float* rays, float* illums, float* lights, size_t
 
     //for (size_t _j = 0; _j < w * h * 6; _j+=6)
     //{
+
+        //for (size_t _j = 0; _j < w * h * 6; _j+=6)
+    //{
+    if(near_ele_num == -1)
+    {
+        image_plane[_i] = 0.0;
+    }
+    else
+    {
+        eid = near_ele_num;
+        ax = nodes[eid];
+        ay = nodes[eid+1];
+        az = nodes[eid+2];
+
+        bx = nodes[eid+3];
+        by = nodes[eid+4];
+        bz = nodes[eid+5];
+
+        cx = nodes[eid+6];
+        cy = nodes[eid+7];
+        cz = nodes[eid+8];
+
+        e01x = bx - ax;
+        e01y = by - ay;
+        e01z = bz - az;
+        
+        e02x = cx - ax;
+        e02y = cy - ay;
+        e02z = cz - az;
+
+        nx = e01y * e02z - e01z * e02y;
+        ny = e01z * e02x - e01x * e02z;
+        nz = e01x * e02y - e01y * e02x;
+
+        srox = rox + rdx * min_dis;
+        sroy = roy + rdy * min_dis;
+        sroz = roz + rdz * min_dis;
+
+        srdx = lx - srox;
+        srdy = ly - sroy;
+        srdz = lz - sroz;
+        
+        ray_length_1 = 1/sqrt(srdx * srdx + srdy * srdy + srdz * srdz);
+        nl_1 = 1/sqrt(nx * nx + ny * ny + nz * nz);
+        // srdx *= ray_length_1;
+        // srdy *= ray_length_1;
+        // srdz *= ray_length_1;
+
+        image_plane[_i] = fabs(srdx * nx + srdy * ny + srdz * nz)*ray_length_1*nl_1;
+    }
+    // return;  
+    __syncthreads();
+
     for(unsigned int _p = 0; _p < elem_size*9; _p+=100*9)
     {
         sz = ((elem_size*9 - _p)>=100*9)?100*9:(elem_size*9 - _p);
@@ -162,67 +221,93 @@ void primary_rays(float* nodes,float* rays, float* illums, float* lights, size_t
             }
         }
         __syncthreads();
-        if(near_ele_num == -1)
+
+        // srox = rox + rdx * min_dis;
+        // sroy = roy + rdy * min_dis;
+        // sroz = roz + rdz * min_dis;
+        
+        // rdx = lx - srox;
+        // rdy = ly - sroy;
+        // rdz = lz - sroz;
+
+        for(unsigned int _r = 0; _r < sz; _r+= 9)
         {
-            image_plane[_i] = 0.0;
-            continue;
-        }
-        else
-        {
-            //t = ts[_j/6];
-            //int eid = near_ele_num;
-            srox = rox + rdx * min_dis;
-            sroy = roy + rdy * min_dis;
-            sroz = roz + rdz * min_dis;
-    
-            for(unsigned int _r = 0; _r < sz; _r+= 9)
+            //eidx = _j + _r;
+            if(_p + _r == near_ele_num)
             {
-                //eidx = _j + _r;
-                ax = elems_shared[_r];
-                ay = elems_shared[_r+1];
-                az = elems_shared[_r+2];
-
-                bx = elems_shared[_r+3];
-                by = elems_shared[_r+4];
-                bz = elems_shared[_r+5];
-
-                cx = elems_shared[_r+6];
-                cy = elems_shared[_r+7];
-                cz = elems_shared[_r+8];
-
-                // nx = nodes[_j + 9];
-                // ny = nodes[_j + 10];
-                // nz = nodes[_j + 11];
-
-                e01x = bx - ax;
-                e01y = by - ay;
-                e01z = bz - az;
-                
-                e02x = cx - ax;
-                e02y = cy - ay;
-                e02z = cz - az;
-
-                nx = e01y * e02z - e01z * e02y;
-                ny = e01z * e02x - e01x * e02z;
-                nz = e01x * e02y - e01y * e02x;
-
-
-                FL_TYPE lx = lights[0];
-                FL_TYPE ly = lights[1];
-                FL_TYPE lz = lights[2];
-
-                
-                rdx = lx - srox;
-                rdy = ly - sroy;
-                rdz = lz - sroz;
-
-                ray_length_1 = 1/sqrt(rdx * rdx + rdy * rdy + rdz * rdz);
-                rdx *= ray_length_1;
-                rdy *= ray_length_1;
-                rdz *= ray_length_1;
-                image_plane[_i] = fabs(rdx * nx + rdy * ny + rdz * nz);
+                // image_plane[0] = 0;
+                continue;
             }
-        }        
+            ax = elems_shared[_r];
+            ay = elems_shared[_r+1];
+            az = elems_shared[_r+2];
+
+            bx = elems_shared[_r+3];
+            by = elems_shared[_r+4];
+            bz = elems_shared[_r+5];
+
+            cx = elems_shared[_r+6];
+            cy = elems_shared[_r+7];
+            cz = elems_shared[_r+8];
+
+            // nx = nodes[_j + 9];
+            // ny = nodes[_j + 10];
+            // nz = nodes[_j + 11];
+
+            e01x = bx - ax;
+            e01y = by - ay;
+            e01z = bz - az;
+            
+            e02x = cx - ax;
+            e02y = cy - ay;
+            e02z = cz - az;
+
+            nx = e01y * e02z - e01z * e02y;
+            ny = e01z * e02x - e01x * e02z;
+            nz = e01x * e02y - e01y * e02x;
+
+            D = - (nx * srdx + ny*srdy + nz*srdz);  // |-d e1 e2| = -n.d
+
+            if(fabs(D) < 1e-6)
+            {
+                continue;
+            }
+
+            tx = srox - ax;
+            ty = sroy - ay;
+            tz = sroz - az;
+
+            Dy = srdx*(tz*e02y - ty*e02z) + tx*(srdy*e02z - e02y*srdz) + e02x*(ty*srdz - srdy*tz);
+            u = Dy / D;
+
+            if(u < 0 || u > 1)
+            {
+                continue;
+            }
+            // ts[_i] = elems_shared[_m];
+
+            Dz = srdx*(e01z*ty - e01y*tz) + e01x*(tz*srdy - ty*srdz) + tx*(e01y*srdz - srdy*e01z);
+
+            v = Dz / D;
+
+            if(v < 0 || v + u > 1)
+            {
+                continue;
+            }
+
+            t = (tx*nx + ty*ny + tz*nz)/D;
+
+            if(t < 0)
+            {
+                continue;
+            }
+            if(t < min_dis)
+            {
+                image_plane[_i] = 0.0;
+                return;
+            }
+        }
+        __syncthreads(); 
     }
 }
 
@@ -271,7 +356,7 @@ int main(int argc, char** argv)
         is_hit[_i] = -1;
     }
     
-    FL_TYPE *drays, *dnodes, *dts;
+    FL_TYPE *drays, *dnodes, *dimage_plane, *dlights;//, *dts;
     FL_TYPE lights[] = {lx, ly, lz};
 
     cudaMalloc((void **)&drays, w*h*6*sizeof(FL_TYPE));
@@ -302,7 +387,7 @@ int main(int argc, char** argv)
     // {
     //     for(size_t _j = 0; _j < w; _j++)
     //     {
-    //         std::cout << ts[_i*w + _j] << " ";
+    //         std::cout << image_plane[_i*w + _j] << " ";
     //     }
     //     std::cout << std::endl;
         
